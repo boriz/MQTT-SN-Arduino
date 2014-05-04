0f6bffe0d56507d8270e6ea3296d993aa376223f
Boris <cherkasskiy@gmail.com>
2014-05-04 13:16:04 -0500

    Serial gateway. Trying to create an API frame to send over MeshBee.

--- serial-mqtts/ser_redir.py
+++ serial-mqtts/ser_redir.py
@@ -3,133 +3,157 @@ import serial
 import sys
 import time
 import datetime
+from struct import *
 
-SerPort = '/dev/ttyAMA0'
-#BrokerHost = "192.168.0.2"
-BrokerHost = "localhost"
+#SerPort = '/dev/ttyAMA0'
+SerPort = 'COM9'
+BrokerHost = "192.168.1.10"
 BrokerPort = 1883
-LocalPort = 1887
-#BrokerHost = "test.mosquitto.org"
-#BrokerPort = 1883
-
+SourceAddress = 0x5DF7
 
 #convert string to hex
-def prnHex(s):
+def ToHex(s):
+	retHex=""
 	for c in s:
-		print "%#04x" % ord(c),
-
+		retHex = retHex + "%#04x " % ord(c)
+	return retHex
 	
-#connect to serial BrokerPort
+#connect to serial SerPort
 def ConnectSer ():
-	global ser
 	try:
-		print "Connecting to Serial ... ", SerPort
-		ser = serial.Serial(SerPort, 115200, timeout=1)
+		print "Connecting to Serial:"+SerPort+" ... ",
+		ser = serial.Serial(SerPort, 115200, timeout=1)		
+		ser.write("ATEX\n")
 		ser.flush()
+		time.sleep(0.1)
+		ser.flushInput()
+		print "Connected"
+		return ser
 	except:
-		print "Failed to connect to serial BrokerPort"
+		print "Failed to connect to serial port"
 		raise SystemExit
+
+# Class to hold connection information
+class conn:
+    def __init__(self):
+		self.sock
+		self.src_addr
+		self.frame_id
 		
-	print "Connected"
-	print
-
-def ConnectUDP ():
-	global sockLocal
-	try:
-		print "Creating UDP Listener ... "
-		sockLocal = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		sockLocal.bind(("0.0.0.0", LocalPort))
-		sockLocal.setblocking(0)
-	except:
-		print "Failed to bind"
-		ser.close()
-		raise SystemExit
-					 
-	print "Connected"
-	print
-
-	
-# def ConnectTCP ():
-	# global sock
-	# try:
-		# host_ip = socket.gethostbyname(BrokerHost)
-		# print "Connecting to TCP server ... ", host_ip
-		# sockLocal = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		# sockLocal.connect((host_ip, BrokerPort))
-		# sockLocal.setblocking(0)
-	# except:
-		# print "Failed to connect TCP server"
-		# ser.close()
-		# raise SystemExit
-					 
-	# print "Connected"
-	# print
-
 	
 # Main loop
-ConnectSer()
-#ConnectTCP()
-ConnectUDP()
-
 data_ser = ""
-data_tcp = ""
+data_udp = ""
+conns = []
+host_ip = socket.gethostbyname(BrokerHost)
+ser = ConnectSer()
+ser.write('\x7E\x00\x11\x90\x00\x00\x00\x00\x00\x00\x00\x00\x06\x07\x00\x01\x02\x03\x04\x05\x60');
 while True:
 	try:
+		# Try to get serial data
 		if ser.inWaiting() > 0:
-			#st=datetime.now()
+			data_ser = ser.read(1)
+			if ord(data_ser[0]) != 0x7E:
+				# Should start with 0x7E
+				print "Wrong frame delimiter:%#04x" % ord(data_ser[0])
+				ser.flushInput()
+				data_ser = ""
+				time.sleep(0.1)
+				continue
+			
+			# Get the rest of the packet
+			print "Frame start detected"
+			time.sleep(0.05)
 			while ser.inWaiting() > 0:
 				data_ser = data_ser + ser.read(1)
-			time.sleep(0.01)
-			continue
+				time.sleep(0.05)
+		
+		# Got serial data
 		if data_ser:
-			if not sockLocal:
-				ConnectTCP()
-				ConnectUDP()
-			print "SER IN>",
-			prnHex(data_ser)
-#			sockLocal.sendall(data_ser)
-			host_ip = socket.gethostbyname(BrokerHost)
-			#sockSend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			sockLocal.sendto(data_ser, (host_ip, BrokerPort))
-			#sockSend.close()
-			print "   -> Sent"
-			data_ser=""
-		
-		#data_tcp = sockLocal.recv(1024)	
-		try:
-			data_tcp, address = sockLocal.recvfrom(1024)
-		except socket.error:	
-			data_tcp=""
-		
-		if data_tcp:
-			print "TCP IN>",
-			prnHex(data_tcp)
-			ser.write(data_tcp) 
-			print "   -> Sent"
-    		
-#		sys.stdin.read()
+			l = len(data_ser)
+			print "Got frame:" + ToHex(data_ser) + "; len:" + str(l)
+			# Find source address (2 bytes)
+			try:			
+				src = unpack('>H', data_ser[12:14])[0]
+				payload = data_ser[15:l-1]
+			except:
+				print "Parsing serial data exception:", sys.exc_info()
+				data_ser=""	
+				continue
+			
+			# Clear serial data
+			data_ser=""			
+			
+			# Parse what seems to be a valid data
+			print "SER Source addr:" + hex(src) 
+			print "SER Payload:" + ToHex(payload)
+			
+			# Try to find a corresponding connection by source address
+			cn = None
+			for i in conns:
+				if i.src_addr == src:
+					cn = i
+					print "SER Found source address:" + "%#04x" % cn.src_addr
+			
+			# Found record?
+			if not cn:
+				# Nope, create a new record
+				cn = conn
+				cn.src_addr = src
+				cn.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+				cn.sock.bind(('',0))
+				cn.sock.setblocking(0)
+				cn.frame_id = 1
+				conns.append(cn)
+				print "SER Create new connection:" + "%#04x" % cn.src_addr + " Port:" + str(cn.sock.getsockname()[1])
+
+			# send serial data to UDP
+			cn.sock.sendto(payload, (host_ip, BrokerPort))
+			print "SER Sent Ok"
+
+		# Check for incoming UDP data
+		for i in conns:
+			try:
+				data_udp = i.sock.recv(256)
+			except:	
+				data_udp=""
+			
+			# Got udp data for client
+			if data_udp:
+				print "UDP input for address:" + "%#04x" % i.src_addr
+				print "UDP payload:" + ToHex(data_udp)				
+				rep = []
+				rep.append(pack('>B', 0x7E))	# Delimiter
+				rep.append(pack('>H', (len(data_udp) + 13)))	# Length, except delimiter and crc 
+				rep.append(pack('B', 0x90))	# API 90				
+				rep.append(pack('>Q', 0x0000000000000000))	# Src addr
+				rep.append(pack('>H', SourceAddress )) # Src addr short
+				rep.append(pack('B', 0x00))	# Options	
+				for x in data_udp:
+					rep.append(pack('B', ord(x)))	# Data	
+				rep.append(pack('B', 0x00))	# Checksum
+				dst = "ATDA" + hex(i.src_addr)[2:]
+				print "UDP Select destination: " + dst
+				ser.write("+++")
+				ser.write(dst + "\n")
+				ser.write("ATEX\n")
+				s = ''.join(rep)
+				ser.write(s)
+				ser.flush()
+				print "UDP sent to serial:" + ToHex(s)
+				time.sleep(0.1)
+				# Probably got a bunch of the OK from AT commands. Discard it
+				ser.flushInput()
+			
 	except KeyboardInterrupt:
 		print "CTRL-C"
 		break
         
-	# except IOError, e: 
-		#error 10054 connection closed
-		# if e.errno == 10054:
-			# print "TCP disconnect exception"
-			# sock.close()
-			# continue
 		
-		#error 10035 is no data available, it is non-fatal
-		# if e.errno != 10035:
-			# print "Failed to send/receive: " + e.strerror
-			# break
-		
-	# except Exception, e:
-		# print "Exception: " + e.strerror
-		# break
-		
-print "Closing Serial and TCP"	
-sockLocal.close()
+print "Closing Serial and UDP"	
 ser.close()
+for i in conns:
+	if i.sock:
+		i.sock.close()
 print "Closed"
 
